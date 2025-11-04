43dd198968b417ec2ad6508e481470ce77619337
TALHA-089 <rajag47786@gmail.com>
2025-11-05 01:37:50 +0500

    Extended to Handle 3 Numbers

--- Sum.py
+++ Sum.py
@@ -1,5 +1,6 @@
 print("Hello from Sum.py")
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-result = a + b
-print(f"The sum of {a} and {b} is {result}")
+c = int(input("Enter third number:"))
+result = a + b + c
+print(f"{a} + {b} + {c} = {result}")
